07d8310b4f2cec4001b88774fbfdcc2f6acd3844
CBurton <burton.chris13@gmail.com>
2023-05-26 11:41:57 -0500

    update reference on readme

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
-import { QUERY_THOUGHTS} from '../utils/queries';
-import ThoughtList from '../components/ThoughtList';
+
 
 
 import Auth from '../utils/auth';
 
 const Home = () => {
   // use useQuery hook to make query request
-  const { loading, data } = useQuery(QUERY_THOUGHTS);
-  
-  const thoughts = data?.thoughts || [];
+   
   const loggedIn = Auth.loggedIn();
   
 
@@ -23,11 +20,9 @@ const Home = () => {
           </div>
         )}
         <div className={`col-12 mb-3 ${loggedIn && 'col-lg-8'}`}>
-          {loading ? (
+          
             <div>Loading...</div>
-          ) : (
-            <ThoughtList thoughts={thoughts} title="Some Feed for Thought(s)..." />
-          )}
+          
         </div>
         
       </div>
