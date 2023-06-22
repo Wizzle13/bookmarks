feeff7a10f41bbb718b490485a0f05783f02cafa
CBurton <burton.chris13@gmail.com>
2023-06-22 17:35:31 -0500

    created card for catagory links

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -18,10 +18,10 @@ const Header = () => {
         {/* https://www.npmjs.com/package/react-burger-menu */}
         <Menu right>
           <a id="home" className="menu-item" href="/">Home</a>
-          <a id="add" className="menu-item" href="/">+ Add Bookmark</a>
-          <a id="profile" className="menu-item" href="/about">Profile</a>
+          <a id="add" className="menu-item" href="/addBookmark">+ Add Bookmark</a>
+          <a id="profile" className="menu-item" href="/profile">Profile</a>
           <a id="about" className="menu-item" href="/about">About</a>
-          <a id="log out" className="menu-item" href="/contact">Logout</a>
+          <a id="log out" className="menu-item" href="/login">Logout</a>
 
         </Menu>
         
--- client/src/components/bookmarklist.js
+++ client/src/components/bookmarklist.js
@@ -1,18 +1,23 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 const BookmarkList = ({ thoughts, title }) => {
-//   if (!thoughts.length) {
-//     return <h3>No Bookmarks Yet</h3>;
-//   }
+  // if (!thoughts.length) {
+  //   return <h3>No Bookmarks Yet</h3>;
+  // }
 
   return (
-    <div>
+    <div className='card'>
         <p className="card-header">
             Catagoty
         </p>
         <div className="card-body">
-            Bookmarks
+            <ul>
+              <li><a href="https://idp.bootcampspot.com/ui/?requestId=a529657e-536e-481e-a3a1-d915235a0379" class="card-link"><img src="https://courses.bootcampspot.com/favicon.ico" width="32px" alt="FavIcon" /> Boot Camp Login</a></li>
+              <li ><a href="https://umn.bootcampcontent.com/" class="card-link"><img src="https://umn.bootcampcontent.com/favicon.ico" width="32px" alt="FavIcon" /> GitLab </a></li>
+              <li><a href="https://github.com/" class="card-link"><img src="https://github.com/favicon.ico" width="32px" alt="FavIcon" /> GitHub</a></li>
+              <li><a href="https://stackoverflow.com/" class="card-link"><img src="https://stackoverflow.com/favicon.ico" alt="FavIcon" width="32px" /> Stack Over Flow</a></li>
+              <li><a href="https://www.favicon.cc/" class="card-link"><img src="https://www.favicon.cc/favicon.ico" alt="FavIcon" width="32px" /> FavIcon</a></li>
+            </ul>
         </div>
           
     </div>
