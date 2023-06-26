73a5f5b3a979585e1efc985ede3cea00956bfa37
CBurton <burton.chris13@gmail.com>
2023-06-26 16:17:13 -0500

    created Seed files

---
+++ server/seeds/catagory-seeds.js
@@ -0,0 +1,44 @@
+// just to basic lay out
+
+const catagoryData = [
+    {
+        user: 1,
+        catagotyName: 'U Of M Codding Camp'
+    },
+    {
+        user: 1,
+        catagotyName: 'Coding Resources'
+    },
+    {
+        user: 1,
+        catagotyName: 'Class Resources'
+    },
+    {
+        user: 1,
+        catagotyName: '3-D Printing'
+    },
+    {
+        user: 1,
+        catagotyName: 'Streaming'
+    },
+    {
+        user: 1,
+        catagotyName: 'Social Media'
+    },
+    {
+        user: 1,
+        catagotyName: 'Banking / Billing'
+    },
+    {
+        user: 1,
+        catagotyName: 'Games'
+    },
+    {
+        user: 1,
+        catagotyName: 'Extras'
+    },
+    {
+        user: 1,
+        catagotyName: 'Jobs'
+    }
+]    
---
+++ server/seeds/user-seeds.js
@@ -0,0 +1,11 @@
+// just to basic lay out
+
+const usersData = [
+    {
+        userName: 'wizzle13',
+        firstName: 'Chris',
+        lastName: ' ',
+        email: 'wizzle13@gmail.com',
+        password: '',
+    }
+]    
